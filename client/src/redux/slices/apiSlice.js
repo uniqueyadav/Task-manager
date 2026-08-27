d7091816899a0689d417ec42d65f8ef604dfcead
Amit Yadav <yadavamit0186@gmail.com>
2026-08-27 17:12:24 +0530

    update

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URI = "http://localhost:8800/api";
+const API_URI = "https://task-manager-ktt8.onrender.com/api";
 
 const baseQuery = fetchBaseQuery({
     baseUrl: API_URI,
