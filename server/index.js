c846405c1e6fed29f3b5fea20af75d97794f9ac0
Amit Yadav <yadavamit0186@gmail.com>
2026-08-27 17:42:21 +0530

    update

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:5173"],
+        origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:5173", "https://cloudebasedtaskmanager.vercel.app"],
         methods: ["GET", "POST", "DELETE", "PUT"],
         credentials: true,
     })
